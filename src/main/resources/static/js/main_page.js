9b59fdc3e6c915d80bb0b52be5ff50b1477f61a9
nikitaln <nikitaln7@gmail.com>
2024-03-13 11:16:01 +0300

    JQuery added

---
+++ src/main/resources/static/js/main_page.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('#btn-show-form').click(function(){
+        $('.task-form').css('display', 'flex');
+    });
+
+    $('#btn-save').click(function(){
+        $('.task-form').css('display', 'none');
+    });
+
+});
