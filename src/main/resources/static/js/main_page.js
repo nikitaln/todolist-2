0c8ae60822076a7ef254ec38cd710df6938d459f
nikitaln <nikitaln7@gmail.com>
2024-04-05 16:35:44 +0300

    js-file fixed

--- src/main/resources/static/js/main_page.js
+++ src/main/resources/static/js/main_page.js
@@ -3,19 +3,10 @@ $(function(){
         $('.task-form').css('display', 'flex');
     });
 
-    $('#btn-save').click(function(){
-        $('.task-form').css('display', 'none');
-    });
-
-    $('#btn-list-tasks').click(function(){
-        $('.task-form').css('display', 'none');
-        $('.list-all-users').css('display', 'none');
-        $('.main-list-tasks').css('display', 'flex');
-    });
-
-    $('#btn-show-users').click(function(){
-        $('.task-form').css('display', 'none');
-        $('.main-list-tasks').css('display', 'none');
-        $('.list-all-users').css('display', 'flex');
+    $(document).mouseup(function (e){ // событие клика по странице
+        if (!$('.task-form').is(e.target) && // если клик сделан не по элементу
+            $('.task-form').has(e.target).length === 0) { // если клик сделан не по вложенным элементам
+            $('.task-form').css('display', 'none');// скрываем блок
+        }
     });
 });
